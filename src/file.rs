2807e2b421c815398f2572db5c2195cd981daa0b
YJ <jun.yang@target-energysolutions.com>
2024-03-29 23:14:23 +0800

    feat: save image utility function

---
+++ src/file.rs
@@ -0,0 +1,12 @@
+use std::path::Path;
+
+use crate::image::Image;
+use crate::image_encoder::Encoder;
+pub fn save_image<P: AsRef<Path>, E: Encoder, I: crate::image::Image>(
+  path: P,
+  img: &I,
+  encoder: E,
+) -> std::io::Result<()> {
+  let encoded = encoder.encode(img);
+  std::fs::write(path, encoded)
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 pub mod geometry;
 pub mod image;
 pub mod image_encoder;
+pub mod file;
