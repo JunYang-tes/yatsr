9421712a6dbb6f47a5871a967db0b8e41a4ec447
YJ <jun.yang@target-energysolutions.com>
2024-04-24 17:53:47 +0800

    feat: cut by z

--- src/pipeline2.rs
+++ src/pipeline2.rs
@@ -94,12 +94,16 @@ fn draw_triangle<M: crate::model::Model, S: Shader<M>, I: Image>(
         let p = a * alpha + b * beta + c * gamma;
         let index = (y * img.width() + x) as usize;
         let k = 1. / wa * alpha + 1. / wb * beta + 1. / wc * gamma;
-        if p.z > depth_buff[index] {
+        if p.z > depth_buff[index] && p.z <= 1.1 && p.z >= -1.1 {
           // 通过Fragment shader 计算每个像素的颜色
           match shader.fragment(p, Vec3::new(alpha / wa / k, beta / wb / k, gamma / wc / k)) {
             Fragment::Color(c) => {
               depth_buff[index] = p.z;
-              img.set_rgb(x, y, c);
+              if p.z >= -1.1 && p.z <= 1.1 {
+                img.set_rgb(x, y, c * 2.);
+              } else {
+                //img.set_rgb(x, y, c);
+              }
             }
             Fragment::Rgba(c) => {
               depth_buff[index] = p.z;
