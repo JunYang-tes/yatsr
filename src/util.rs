f61e87c6fa949f30b342e24a265c6b8f5f4b179f
Yang Jun <jun.yang@target-energysolutions.com>
2024-05-02 10:41:03 +0800

    feat: add sub_img and intersection of plane and line

--- src/util.rs
+++ src/util.rs
@@ -1,3 +1,5 @@
+use crate::prelude::{Vec3, PixImage, Image};
+
 pub fn load_image<P: AsRef<std::path::Path>>(p: P) -> crate::image::PixImage {
   use crate::image_decoder::*;
   let tga = TGA;
@@ -9,4 +11,36 @@ where
   T: Copy + std::ops::Sub<Output = T> + std::ops::Mul<S, Output = T> + std::ops::Add<Output = T>,
 {
   a + (b - a) * t
+}
+
+/*
+ * 计算射线和平面点交点
+ * */
+pub fn intersect_of_plan_line(
+  p: Vec3<f32>,  //平面上一点
+  n: Vec3<f32>,  //平面法向量
+  p0: Vec3<f32>, // 射线顶点
+  d: Vec3<f32>,  // 射线方向
+) -> Option<Vec3<f32>> {
+  let t = (p * n - p0 * n) * (1. / (d * n));
+  if t > 0. {
+    Some(p0 + (d * t))
+  } else {
+    None
+  }
+}
+
+pub fn sub_img(img: &PixImage, x: f32, y: f32, w: f32, h: f32) -> PixImage {
+  let mut sub = PixImage::new(
+    (w * img.width() as f32) as u32,
+    (h * img.height() as f32) as u32,
+  );
+  let x = (x * img.width() as f32) as u32;
+  let y = (y * img.height() as f32) as u32;
+  for c in 0..sub.width() {
+    for r in 0..sub.height() {
+      sub.set_rgba32(c, r, img.get_rgba(x + c, y + r))
+    }
+  }
+  sub
 }
